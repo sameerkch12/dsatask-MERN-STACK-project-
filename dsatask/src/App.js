a61bd57b6eaab3d8c86945588e39326864cb4221
sameerkch12 <sameer.sc62@gmail.com>
2023-07-16 23:01:57 -0700

    sameer- Login,signup,footer,navbar filr add

--- dsatask/src/App.js
+++ dsatask/src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 import Home from './Screen/Home';
+import Login from './Screen/Login';
+import SignUp from './Screen/SignUp';
 import UpcomingContest from './Screen/UpcomingContest';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
         <div>
           <Routes>
             <Route exact path='/' element={<Home />} />
+            <Route exact path='/login' element={<Login/>} />
+            <Route exact path='/signup' element={<SignUp/>} />
             <Route exact path='/upcomingContest' element={<UpcomingContest/>} />
           </Routes>
         </div>
