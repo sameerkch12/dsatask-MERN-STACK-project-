4703dc49d45ff52c39dc286fec5262d529430dd9
truefuego <ankitsharma15092003@gmail.com>
2023-07-18 03:57:04 +0530

    Update navbar, login, signup page

---
+++ dsatask/src/Screen/Login/Login.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react'
+import './Login.css'
+import { Link } from 'react-router-dom';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform login logic here
+    console.log('Email:', email);
+    console.log('Password:', password);
+    
+    setEmail('')
+    setPassword('')
+
+    // We will have to load the home page with the user data
+    window.location.href = '/home'
+  };
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            className="form-control"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            className="form-control"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <p>Don't have an account? <Link to='/signup' className='icon-link'> Sign Up</Link></p>
+        <button type="submit" className="btn btn-primary text-align-center">Login</button>
+      </form>
+    </div>
+  )
+}
+
--- dsatask/src/components/Navbar/Navbar.js
+++ dsatask/src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Navbar.css'; // Import the CSS file for styling
 import { Link } from 'react-router-dom';
 
-const Navbar = (props) => {
+const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light" >
       <h1 className="navbar-brand">Navbar</h1>
@@ -11,14 +11,11 @@ const Navbar = (props) => {
       </button>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav ml-auto">
-          {props.isLoggedIn && (<Link to="/" className="nav-item nav-link">Home</Link>)}
-          {props.isLoggedIn && (<Link to="/register" className="nav-item nav-link">Sign Out</Link>)}
-          {!props.isLoggedIn && (<Link to="/login" className="nav-item nav-link">Login</Link>)}
-          {!props.isLoggedIn && (<Link to="/signup" className="nav-item nav-link">SignIn</Link>)}
-          {props.isLoggedIn && (<Link to="/upcomingContest" className="nav-item nav-link">Upcoming Contest</Link>)}
+          <Link to="/home" className="nav-item nav-link">Home</Link>
+          <Link to="/" className="nav-item nav-link">Sign Out</Link>
+          <Link to="/upcomingContest" className="nav-item nav-link">Upcoming Contest</Link>
         </div>
       </div>
-      <div className="dropdown-divider"></div>
     </nav>
   );
 }
