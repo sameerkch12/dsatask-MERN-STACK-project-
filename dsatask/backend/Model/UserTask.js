6bd6f3dc0a5238edb71eee456cfd03c4462ff041
sameerkch12 <sameer.sc62@gmail.com>
2023-07-17 04:53:09 -0700

    sameer backend create model,route->createtask

---
+++ dsatask/backend/Model/UserTask.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const TaskSchema = new Schema({
+  Problem: String,
+});
+
+const DateSchema = new Schema({
+  Date: Date,
+  Tasks: [TaskSchema],
+});
+
+const UserTaskSchema = new Schema({
+  Email: String,
+  Dates: [DateSchema],
+});
+
+const UserTask = mongoose.model('UserTask', UserTaskSchema);
+
+module.exports = UserTask;
+
+
+/*
+for this data base
+
+[
+  {
+    "Email": "sameer@gmail.com",
+    "Dates": [
+      {
+        "Date": "2023-12-20",
+        "Tasks": [
+          {
+            "Problem": "problem 1"
+          },
+          {
+            "Problem": "problem 2"
+          },
+          {
+            "Problem": "problem 3"
+          }
+        ]
+      },
+      {
+        "Date": "2023-12-22",
+        "Tasks": [
+          {
+            "Problem": "problem 3"
+          },
+          {
+            "Problem": "problem 4"
+          },
+          {
+            "Problem": "problem 5"
+          }
+        ]
+      }
+    ]
+  },
+
+
+  {
+    "Email": "bobo@gmail.com",
+    "Dates": [
+      {
+        "Date": "2023-12-28",
+        "Tasks": [
+          {
+            "Problem": "problem 1"
+          },
+          {
+            "Problem": "problem 2"
+          },
+          {
+            "Problem": "problem 3"
+          }
+        ]
+      }
+    ]
+  }
+]
+
+
+*/
---
+++ dsatask/backend/Routes/CreateTask.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const UserTask = require('../Model/UserTask');
+
+
+router.post('/createtask', async (req, res) => {
+  try {
+    const { Email, BodyDate, Problem } = req.body;
+
+    let userTask = await UserTask.findOne({ Email });
+
+    if (userTask) 
+    {
+      // Check if the user's email already exists in the database
+      let existingDate = null;
+      for (let i = 0; i < userTask.Dates.length; i++) {
+        if (userTask.Dates[i].Date === BodyDate) {
+          existingDate = userTask.Dates[i];
+          console.log(existingDate)
+          break;
+        }
+      }
+
+      if (existingDate) {
+        // If the date already exists, push the problem into the existing date's tasks array
+        existingDate.Tasks.push({ Problem });
+      }
+      
+      else {
+        // If the date doesn't exist, create a new date with the task and push it into the dates array
+        userTask.Dates.push({
+          Date: BodyDate,
+          Tasks: [{ Problem }],
+        });
+      }
+    }
+    
+    else
+    {
+      // If the user doesn't exist, create a new user task with the date and task
+      userTask = new UserTask({
+        Email,
+        Dates: [
+          {
+            Date: BodyDate,
+            Tasks: [{ Problem }],
+          },
+        ],
+      });
+    }
+
+    await userTask.save();
+
+    res.status(201).json({ success: true, message: 'User task created successfully' });
+  } catch (error) {
+    res.status(500).json({ success: false, error: 'Failed to create user task' });
+  }
+});
+
+module.exports = router;
