0e5367aca5ba11d5a4fa8c11164af004f51d9d93
sameerkch12 <sameer.sc62@gmail.com>
2023-07-23 02:33:10 -0700

    Today Task Working

--- dsatask/src/Screen/Home/Home.js
+++ dsatask/src/Screen/Home/Home.js
@@ -1,4 +1,7 @@
-import React from 'react'
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from "react-router-dom";
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 import Task from '../../components/Task/Task'
@@ -6,32 +9,103 @@ import './Home.css'
 import TodayCard from '../../components/TodayCard/TodayCard'
 import CreateTask from '../../components/CreateNewTask/CreateNewTask'
 import Divider from '../../components/Divider/Divider'
-import { Link } from 'react-router-dom'
+import ProblemSection from '../../components/TaskDisplayCode/ProblemSection';
 export default function Home() {
-  const Email = localStorage.getItem("email")
+
+  const [todaytaskContestData, settodaytaskContestData] = useState([]);
+  const [BodyDate, setDate] = useState(new Date());
+  const [Email, setEmail] = useState(""); // No default value here
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Load the email from localStorage if it exists
+    const storedEmail = localStorage.getItem("email");
+    if (storedEmail) {
+      setEmail(storedEmail);
+    }
+  }, []);
+
+
+  useEffect(() => {
+    // Save the Email value to localStorage whenever it changes
+    localStorage.setItem("email", Email);
+  }, [Email]);
+
+
+  useEffect(() => {
+    console.log("Email:", Email);
+    console.log("BodyDate:", BodyDate);
+
+    // Call the API only when both Email and BodyDate are available
+    if (Email && BodyDate) {
+      getData();
+    }
+  }, [Email, BodyDate]);
+
+
+  function getData() {
+    setLoading(true);
+    axios
+      .get("http://localhost:5000/api/todaytask", {
+        params: {
+          Email: Email,
+          BodyDate: BodyDate.toISOString().slice(0, 10), // Format the date as 'YYYY-MM-DD'
+        }
+      })
+      .then((res) => {
+        console.log("API Response:", res.data);
+        settodaytaskContestData(res.data.tasks);
+        setLoading(false); // API call is finished
+      })
+      .catch((error) => {
+        console.error("API Error:", error);
+        setLoading(false); // API call is finished (even if there's an error)
+      });
+  }
+
+  if (!Email) {
+    navigate('/signup');
+    return null; // Render nothing until the redirect happens
+  }
+
+
+  console.log("todaytask:", todaytaskContestData);
 
   return (
     <div>
-      <Navbar/>
+      <Navbar />
 
 
       <div className='container'>
         <div className='time-add'>
-          <TodayCard name={Email}/>
-          {Email === null ? 
-            (<Link to='/login'>Login Here</Link>):
-            (<CreateTask email={Email}/>)
+          <TodayCard name={Email} />
+          {Email === null ?
+            (<Link to='/login'>Login Here</Link>) :
+            (<CreateTask email={Email} />)
           }
         </div>
         <Divider />
-        <div className='item-container'>
-          <Task task="Task Name" link="https://leetcode.com/problems/non-overlapping-intervals/"/>
-          <Task task="Task Name" link="problem_link"/>
-          <Task task="Task Name" link="problem_link"/>
-          <Task task="Task Name" link="problem_link"/>
-          <Task task="Task Name" link="problem_link"/>
-        </div>
+
       </div>
+      <p>Number of Today Contests: {todaytaskContestData.length}</p>
+      <div className="grid-container">
+        {loading ? (
+          <p>Loading...</p>
+        ) : todaytaskContestData.length > 0 ? (
+          todaytaskContestData.map((task, index) => (
+            <ProblemSection
+              key={index}
+              link={task.Problem}
+            />
+          ))
+        ) : (
+          <p>No upcoming contests found.</p>
+        )}
+      </div>
+
+
+
       <Footer />
     </div>
   )
