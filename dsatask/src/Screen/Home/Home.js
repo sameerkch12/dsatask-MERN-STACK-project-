977f38eb0173af09e8f1c6b3b30778e13d30546c
truefuego <ankitsharma15092003@gmail.com>
2023-07-23 20:10:07 +0530

    add task working fine today tas rendering on task creation

--- dsatask/src/Screen/Home/Home.js
+++ dsatask/src/Screen/Home/Home.js
@@ -7,7 +7,7 @@ import Footer from '../../components/Footer/Footer'
 import Task from '../../components/Task/Task'
 import './Home.css'
 import TodayCard from '../../components/TodayCard/TodayCard'
-import CreateTask from '../../components/CreateNewTask/CreateNewTask'
+// import CreateTask from '../../components/CreateNewTask/CreateNewTask'
 import Divider from '../../components/Divider/Divider'
 import ProblemSection from '../../components/TaskDisplayCode/ProblemSection';
 export default function Home() {
@@ -17,6 +17,9 @@ export default function Home() {
   const [Email, setEmail] = useState(""); // No default value here
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+  const [taskLink,setTaskLink] = useState('');
+  const [taskDate,setTaskDate] = useState('');
+
 
   useEffect(() => {
     // Load the email from localStorage if it exists
@@ -34,15 +37,11 @@ export default function Home() {
 
 
   useEffect(() => {
-    console.log("Email:", Email);
-    console.log("BodyDate:", BodyDate);
-
     // Call the API only when both Email and BodyDate are available
     if (Email && BodyDate) {
       getData();
     }
   }, [Email, BodyDate]);
-
 
   function getData() {
     setLoading(true);
@@ -68,9 +67,37 @@ export default function Home() {
     navigate('/signup');
     return null; // Render nothing until the redirect happens
   }
+  const taskDateChangeHandler = (e) => {
+    setTaskDate(e.target.value)
+  }
 
+  const taskLinkChangeHandler = (e) => {
+    setTaskLink(e.target.value)
+  }
 
-  console.log("todaytask:", todaytaskContestData);
+  const createTaskHandler = async(e) => {
+    e.preventDefault()
+    
+    // Create newData
+    const newData = {
+      Email, 
+      BodyDate: taskDate, 
+      Problem: taskLink,
+    }
+
+    await axios
+      .post('http://localhost:5000/api/createtask',newData)
+      .then((res) =>{
+        console.log(res);
+        getData()
+      })
+      .catch((err) => {
+        console.log(err)
+        console.log("Server Error")
+      })
+      setTaskDate('')
+      setTaskLink('')
+    }
 
   return (
     <div>
@@ -82,27 +109,39 @@ export default function Home() {
           <TodayCard name={Email} />
           {Email === null ?
             (<Link to='/login'>Login Here</Link>) :
-            (<CreateTask email={Email} />)
+            (
+              <div className='task-create-item'>
+                <div className='upper-task-create-data'>
+                  <input value={taskLink} className='input-create-data' type='text' placeholder='link...' onChange={taskLinkChangeHandler}/>
+                </div>
+                <div className='lower-task-create-data'>  
+                  <div>
+                    <input value={taskDate} className='input-date-data' type='date' placeholder='Date' onChange={taskDateChangeHandler}/>
+                  </div>    
+                  <button className='input-create-button' onClick={createTaskHandler}>Add Task</button>
+                </div>
+              </div>
+            )
           }
         </div>
         <Divider />
 
-      </div>
-      <p>Number of Today Contests: {todaytaskContestData.length}</p>
-      <div className="grid-container">
-        {loading ? (
-          <p>Loading...</p>
-        ) : todaytaskContestData.length > 0 ? (
-          todaytaskContestData.map((task, index) => (
-            <ProblemSection
-              key={index}
-              link={task.Problem}
-            />
-          ))
-        ) : (
-          <p>No upcoming contests found.</p>
-        )}
-      </div>
+        </div>
+        <p>Number of Today Contests: {todaytaskContestData.length}</p>
+        <div className="grid-container">
+          {loading ? (
+            <p>Loading...</p>
+          ) : todaytaskContestData.length > 0 ? (
+            todaytaskContestData.map((task, index) => (
+              <ProblemSection
+                key={index}
+                link={task.Problem}
+              />
+            ))
+          ) : (
+            <p>No tasks for today</p>
+          )}
+        </div>
 
 
 
