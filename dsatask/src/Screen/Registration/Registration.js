2841f455d04cbaa98885b201b20b69032951c345
Sameer Choudhary <62991891+sameerkch12@users.noreply.github.com>
2023-09-05 12:08:42 -0700

    add registration button

--- dsatask/src/Screen/Registration/Registration.js
+++ dsatask/src/Screen/Registration/Registration.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import './Registration.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './Registration.css';
+import { Link } from 'react-router-dom';
 
 export default function Registration() {
-  localStorage.setItem('loggedIn',false)
-  return (
-    <div>
-      Register (---) 
-      <Link to='/login'>Login</Link> (---)
-      <Link to='/signup'>Sign Up</Link>
-    </div>
-  )
-}
+  localStorage.setItem('loggedIn', false);
 
+  return (
+    <div className="registration-container">
+      <h2>Register</h2>
+      <div className="buttons-container">
+        <Link to='/login' className="button">Login</Link>
+        <Link to='/signup' className="button">Sign Up</Link>
+      </div>
+    </div>
+  );
+}
