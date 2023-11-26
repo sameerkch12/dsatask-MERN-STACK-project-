6851e615a2c1f7a90f20d9a801ded9445c5f7a2a
sameerkch12 <sameer.sc62@gmail.com>
2023-11-26 15:52:47 +0530

    Your commit message here

--- dsatask/src/components/Navbar/Navbar.js
+++ dsatask/src/components/Navbar/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light" >
-      <h1 className="navbar-brand">Navbar</h1>
+      <h1 className="navbar-brand">DSATASK</h1>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
